10f9a110b77d58f853b0e42cd227ecf450d71551
Vijayarajan Sangeetha Vidhya Harini <128467792+vidhyaharini0@users.noreply.github.com>
2026-07-11 20:05:02 +0200

    Create connect.php

---
+++ components/connect.php
@@ -0,0 +1,9 @@
+<?php
+
+$db_name = 'mysql:host=localhost;dbname=papas_pizzeria';
+$user_name = 'papasuser';
+$user_password = 'papas1975';
+
+$conn = new PDO($db_name, $user_name, $user_password);
+
+?>
